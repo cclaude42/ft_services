78a50fc6e189e0c1294b3004726c588a41185f3b
cclaude <cesarclaudepro@gmail.com>
2020-09-15 18:06:14 +0200

    added logins.txt

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'admin' );
+define( 'DB_USER', 'wordpress_admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', '1010' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mysql' );
