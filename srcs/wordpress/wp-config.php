e673c4c05ada901eab4af76cecb315a323a4af89
cclaude42 <cesarclaudepro@gmail.com>
2020-10-01 17:40:02 +0200

    tweaked stuff

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wp_admin' );
 define( 'DB_PASSWORD', '1010' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', 'mysql-service' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
