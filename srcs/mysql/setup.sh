4d6239ddf1f9ae90b5d57a83f4e28f7857eeb964
cclaude42 <cesarclaudepro@gmail.com>
2020-10-01 14:58:56 +0200

    fixed typo in mysql setup

--- srcs/mysql/setup.sh
+++ srcs/mysql/setup.sh
@@ -1,5 +1,5 @@
 # Setup MySQL
-/usr/bin/mysql_install_db --user=root -datadir=/var/lib/mysql
+/usr/bin/mysql_install_db --user=root --datadir=/var/lib/mysql
 
 # Start MySQL
-/usr/bin/mysqld --user=root -datadir=/var/lib/mysql --console --init_file=mysql_init
+/usr/bin/mysqld --user=root --datadir=/var/lib/mysql --init_file=mysql_init --console
