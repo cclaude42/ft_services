9a3caeb57590934ee6943ed68cd0b4f950b74087
cclaude42 <cesarclaudepro@gmail.com>
2020-10-02 17:57:10 +0200

    removed wordpress debug

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -80,7 +80,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy publishing. */
 
