a2d6b4f9461356ba218533e5c07a4bc9d24fb56a
cclaude42 <cesarclaudepro@gmail.com>
2020-10-05 23:40:57 +0200

    polished mysql setup

--- srcs/mysql/setup.sh
+++ srcs/mysql/setup.sh
@@ -1,8 +1,11 @@
 # Setup MySQL
 /usr/bin/mysql_install_db --datadir=/var/lib/mysql
 
-# Start MySQL
-/usr/bin/mysqld --user=root --init_file=/init_file &
-sleep 5
+# Start MySQL in background
+/usr/bin/mysqld --user=root --init_file=/init_file & sleep 3
+
+# Initialize database
 mysql wordpress -u root < wordpress.sql
+
+# Keep container running
 tail -f /dev/null
