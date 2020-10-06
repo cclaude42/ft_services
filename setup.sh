80d41d75d21ea6cc39a78e032e3c080f281df824
cclaude42 <cesarclaudepro@gmail.com>
2020-10-06 14:20:25 +0200

    separated vm setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,5 @@
 #!/bin/bash -e
 
-vm_setup()
-{
-	if [ ! -f ~/.vm_setup ];
-	then
-		echo "Setting up VM..."
-		sudo apt update &> /dev/null
-		echo "Updating kubectl..."
-		sudo curl -L "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl &> /dev/null
-		echo "Updating minikube..."
-		sudo curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube &> /dev/null
-		sudo chmod +x /usr/local/bin/kubectl /usr/local/bin/minikube &> /dev/null
-		echo "Setting docker user..."
-		sudo usermod -aG docker user42 && newgrp docker &
-		sleep 1
-		touch ~/.vm_setup
-		echo "VM is set !"
-	fi
-}
-
 minikube_setup()
 {
 	minikube delete
@@ -84,9 +65,6 @@ custom()
 			image_build $i
 			deployment_build $i
 			service_build $i
-		elif [ $i = vm ]
-		then
-			vm_setup
 		elif [ $i = minikube ]
 		then
 			minikube_setup
@@ -105,7 +83,6 @@ custom()
 
 main()
 {
-	vm_setup
 	minikube_setup
 	images
 	deployments
---
+++ srcs/vm_setup.sh
@@ -0,0 +1,21 @@
+vm_setup()
+{
+	if [ ! -f ~/.vm_setup ];
+	then
+		echo "Setting up VM..."
+		sudo apt update &> /dev/null
+		echo "Updating kubectl..."
+		sudo curl -L "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl &> /dev/null
+		echo "Updating minikube..."
+		sudo curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube &> /dev/null
+		sudo chmod +x /usr/local/bin/kubectl /usr/local/bin/minikube &> /dev/null
+		echo "Setting docker user..."
+		touch ~/.vm_setup
+		sudo usermod -aG docker user42 && newgrp docker
+		echo "VM is set !"
+	else
+		echo "VM is already set !"
+	fi
+}
+
+vm_setup
