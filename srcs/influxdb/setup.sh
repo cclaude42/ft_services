c5e98eaabf6801319d6c9ebd1a336536cc697a02
cclaude42 <cesarclaudepro@gmail.com>
2020-10-08 17:44:00 +0200

    forgot tail dev null lmao

--- srcs/influxdb/setup.sh
+++ srcs/influxdb/setup.sh
@@ -5,3 +5,6 @@
 influx -execute "CREATE DATABASE grafana"
 influx -execute "CREATE USER graf_admin WITH PASSWORD '10101'"
 influx -execute "GRANT ALL ON grafana TO graf_admin"
+
+# Keep container running
+tail -f /dev/null
