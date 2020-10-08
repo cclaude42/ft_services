3d09656a597543313e7cabf993d043c20a868b99
cclaude42 <cesarclaudepro@gmail.com>
2020-10-08 18:13:22 +0200

    attempting to configure grafana with conf/provisioning

--- setup.sh
+++ setup.sh
@@ -121,3 +121,21 @@ then
 else
 	main
 fi
+
+echo -e "Default \e[30mBlack"
+echo -e "Default \e[31mRed"
+echo -e "Default \e[32mGreen"
+echo -e "Default \e[33mYellow"
+echo -e "Default \e[34mBlue"
+echo -e "Default \e[35mMagenta"
+echo -e "Default \e[36mCyan"
+echo -e "Default \e[37mLight gray"
+echo -e "Default \e[90mDark gray"
+echo -e "Default \e[91mLight red"
+echo -e "Default \e[92mLight green"
+echo -e "Default \e[93mLight yellow"
+echo -e "Default \e[94mLight blue"
+echo -e "Default \e[95mLight magenta"
+echo -e "Default \e[96mLight cyan"
+echo -e "Default \e[97mWhite"
+echo -e "Default \e[39mDefault"
