0826a007bd54063405dbe5446c53362095cfbd8d
cclaude42 <cesarclaudepro@gmail.com>
2020-10-09 21:45:14 +0200

    finishing setup

--- setup.sh
+++ setup.sh
@@ -1,50 +1,48 @@
 #!/bin/bash -e
 
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+BLUE="\e[94m"
+PURPLE="\e[95m"
+CYAN="\e[96m"
+WHITE="\e[97m"
+
 banner()
 {
-	echo -e "\e[35m"
-	echo "##############################################################################################"
-	echo -e "\e[96m"
-	echo "8888888888 88888888888                                          d8b                           "
-	echo "888            888                                              Y8P                           "
-	echo "888            888                                                                            "
-	echo "8888888        888           .d8888b   .d88b.  888d888 888  888 888  .d8888b .d88b.  .d8888b  "
-	echo "888            888           88K      d8P  Y8b 888P\"   888  888 888 d88P\"   d8P  Y8b 88K      "
-	echo "888            888           \"Y8888b. 88888888 888     Y88  88P 888 888     88888888 \"Y8888b. "
-	echo "888            888                X88 Y8b.     888      Y8bd8P  888 Y88b.   Y8b.          X88 "
-	echo "888            888   88888888 88888P'  \"Y8888  888       Y88P   888  \"Y8888P \"Y8888   88888P' "
-	echo -e "\e[35m"
-	echo "##############################################################################################"
-	echo -e "\e[97m"
+	echo -e $PURPLE
+	echo "###############################################################################################"
+	echo -e $CYAN
+	echo " 8888888888 88888888888                                          d8b                           "
+	echo " 888            888                                              Y8P                           "
+	echo " 888            888                                                                            "
+	echo " 8888888        888           .d8888b   .d88b.  888d888 888  888 888  .d8888b .d88b.  .d8888b  "
+	echo " 888            888           88K      d8P  Y8b 888P\"   888  888 888 d88P\"   d8P  Y8b 88K      "
+	echo " 888            888           \"Y8888b. 88888888 888     Y88  88P 888 888     88888888 \"Y8888b. "
+	echo " 888            888                X88 Y8b.     888      Y8bd8P  888 Y88b.   Y8b.          X88 "
+	echo " 888            888   88888888 88888P'  \"Y8888  888       Y88P   888  \"Y8888P \"Y8888   88888P' "
+	echo -e $PURPLE
+	echo "###############################################################################################"
+	echo -e $WHITE
 }
 
-image_build()
+footer()
 {
-	echo -e "\e[95mBuilding $1 image...\e[93m"
-	docker build -t ${1}_alpine srcs/$1/. | grep "Step"
-	echo -e "\e[32mSuccessfully built $1 image !\n"
-}
-
-deployment_build()
-{
-	kubectl apply -f srcs/yaml_deployments/$1.yaml &> /dev/null
-	echo -e "\e[32mSuccessfully deployed $1 !"
-}
-
-volume_build()
-{
-	kubectl apply -f srcs/yaml_volumes/$1.yaml &> /dev/null
-	echo -e "\e[32mSuccessfully created $1 volume !"
-}
-
-service_build()
-{
-	kubectl apply -f srcs/yaml_services/$1.yaml &> /dev/null
-	echo -e "\e[32mSuccessfully exposed $1 !"
+	echo -e $GREEN
+	kubectl get svc
+	echo -e $CYAN
+	echo "FT_SERVICES IS READY !"
+	echo "Go to 172.17.0.2 to try it."
+	echo -e $GREEN
+	echo "Services    | Wordpress  | Grafana    | FTPS       | SSH        "
+	echo "Login       | wp_admin   | admin      | ftp_admin  | ssh_admin  "
+	echo "Password    | 1010       | admin      | 01010      | 0101       "
+	echo -e $WHITE
 }
 
 minikube_setup()
 {
+	echo -e $BLUE
 	minikube delete
 	minikube start --driver=docker --cpus=2
 	minikube addons enable metrics-server
@@ -54,6 +52,31 @@ minikube_setup()
 	kubectl apply -f srcs/yaml_metallb/metallb.yaml
 	eval $(minikube docker-env)
 	echo
+}
+
+image_build()
+{
+	echo -e "${WHITE}Building $1 image...$YELLOW"
+	docker build -t ${1}_alpine srcs/$1/. | grep "Step"
+	echo -e "${GREEN}Successfully built $1 image !\n"
+}
+
+deployment_build()
+{
+	kubectl apply -f srcs/yaml_deployments/$1.yaml &> /dev/null
+	echo -e "${BLUE}Successfully deployed $1 !"
+}
+
+volume_build()
+{
+	kubectl apply -f srcs/yaml_volumes/$1.yaml &> /dev/null
+	echo -e "${RED}Successfully created $1 volume !"
+}
+
+service_build()
+{
+	kubectl apply -f srcs/yaml_services/$1.yaml &> /dev/null
+	echo -e "${PURPLE}Successfully exposed $1 !"
 }
 
 images()
@@ -109,7 +132,7 @@ main()
 	deployments
 	volumes
 	services
-	echo -e "\e[97mFt_services is ready !"
+	footers
 }
 
 custom()
